5e53687f9a0505a552fb8cef2e122fd0aad16627
unknown <shaun92168@gmail.com>
2019-04-09 14:31:07 -0700

    change bottom navigation to icons

--- App.js
+++ App.js
@@ -13,12 +13,20 @@ import comments from './app/screens/comments.js';
 
 const mainTabs = createBottomTabNavigator(
   {
-
-    Feed: {
-      screen: feed,
-    },
+    Feed: { screen: feed },
     Upload: { screen: upload },
-    Profile: { screen: profile }
+    Profile: { screen: profile },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+        tabBarIcon: ({ focused, tintColor }) =>
+          getTabBarIcon(navigation, focused, tintColor),
+    }),
+    tabBarOptions: {
+      activeTintColor: '#33ccff',
+      inactiveTintColor: 'gray',
+      showLabel: false
+    }
   }
 )
 
@@ -35,6 +43,23 @@ const MainStack = createStackNavigator(
 
   }
 )
+
+const getTabBarIcon = (navigation, focused, tintColor) => {
+  const { routeName } = navigation.state;
+  let IconComponent = Ionicons;
+  let iconName;
+  if (routeName === 'Feed') {
+    iconName = `ios-home`;
+  } else if (routeName === 'Profile') {
+    iconName = `ios-person`;
+  } else if (routeName === 'Upload') {
+    // iconName = `ios-add-circle${focused ? '' : '-outline'}`;
+    iconName = 'md-cloud-upload';
+  }
+
+  // You can return any component that you like here!
+  return <IconComponent name={iconName} size={25} color={tintColor} />;
+};
 
 const AppContainer = createAppContainer(MainStack);
 
